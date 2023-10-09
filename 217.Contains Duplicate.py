17541d712290e1438139dafc490f5594154dc340
0xp10 <844hsby844@gmail.com>
2023-10-09 22:57:03 +0300

    Array - Easy

---
+++ 217.Contains Duplicate.py
@@ -0,0 +1,40 @@
+'''
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: true
+Example 2:
+
+Input: nums = [1,2,3,4]
+Output: false
+Example 3:
+
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+Output: true
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+-109 <= nums[i] <= 109
+
+'''
+# First solution
+def containsDuplicate(nums:list)-> bool:
+    mySet = set()
+    for i in nums:
+        if i in mySet:
+            return True
+        else:
+            mySet.add(i)
+
+print(containsDuplicate([1,1]))
+
+def containsDuplicate(nums:list)-> bool:
+    return len(nums) != len(set(nums))
+
+print(containsDuplicate([1,1]))
